d722791e9305aeab80e6df98644c10947bb75def
Nguyen Binh An <nguyenbinhanltv@gmail.com>
2021-03-03 16:40:35 +0700

    rm admin-ui

--- src/__admin-ui/src/extension.routes.ts
+++
@@ -1,4 +0,0 @@
-export const extensionRoutes = [  {
-    path: 'extensions/webhook',
-    loadChildren: () => import('./extensions/0f0bb66a01213486cf3aa947e4690d963a1e6a51840ebc3d68b232c7cf6518da/webhook.module').then(m => m.WebhookModule),
-  }];
--- src/__admin-ui/src/shared-extensions.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { WebhookNavModule } from './extensions/0f0bb66a01213486cf3aa947e4690d963a1e6a51840ebc3d68b232c7cf6518da/webhook-nav.module';
-
-
-@NgModule({
-    imports: [CommonModule, WebhookNavModule],
-})
-export class SharedExtensionsModule {}
